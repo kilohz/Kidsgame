71c17a5df1d56a05481904f061a72f386cfc8d71
John <j.f.okennedy@gmail.com>
2021-01-09 17:57:58 +0200

    completed scene1

--- Assets/Scripts/DropSlot.cs
+++ Assets/Scripts/DropSlot.cs
@@ -1,39 +1,83 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.XR.WSA.Input;
 
 public class DropSlot : MonoBehaviour, IDropHandler
 {
-	[SerializeField]
-	public Image state1;
-	[SerializeField]
-	public Image state2;
-	[SerializeField]
-	public Image state3;
 
-	[SerializeField]
-	public string ValidItemTag = "";
+    [SerializeField] private Sprite State1;
+    [SerializeField] private Sprite State2;
+    [SerializeField] private Sprite State3;
+    private int State = 0;
 
-	// Start is called before the first frame update
-	void Start()
+    [SerializeField] public string ValidItemTag = "";
+
+    [SerializeField] public GameObject Particles;
+
+
+
+    // Start is called before the first frame update
+    void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
 
-	public void OnDrop(PointerEventData eventData)
-	{
-		Debug.Log("OnDrop");
-		if (eventData.pointerDrag != null)
-		{
-			//eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
-		}
-	}
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("OnDrop");
+        if (eventData.pointerDrag != null && eventData.pointerDrag.gameObject.tag == ValidItemTag)
+        {
+            State++;
+            Destroy(eventData.pointerDrag.gameObject);
+            //eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+
+            UpdateSprite();
+
+            //showEffects
+            StartCoroutine(ShowParticleEffects());
+        }
+    }
+
+    private IEnumerator ShowParticleEffects()
+    {
+        var effects = this.Particles.GetComponent<ParticleSystem>();
+        //if (!effects.isPlaying)
+        //{
+            effects.Play();
+            yield return new WaitForSeconds(0.5f);
+            effects.Stop();
+        //}
+    }
+
+    private void UpdateSprite()
+    {
+        Sprite sprite = null;
+        switch (State)
+        { 
+            case 0:
+                sprite = State1;
+            break;
+            case 1:
+                sprite = State2;
+                break;
+            case 2:
+                sprite = State3;
+                break;
+            default:
+                sprite = State1;
+                break;
+        }
+        this.GetComponent<Image>().sprite = sprite;
+    }
 }
---
+++ Assets/Scripts/TextHueShifter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TextHueShifter : MonoBehaviour
+{
+    public float Speed = 1;
+    private Text text;
+
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        text.color = HSBColor.ToColor(new HSBColor(Mathf.PingPong(Time.time * Speed, 1), 1, 1));
+    }
+}
